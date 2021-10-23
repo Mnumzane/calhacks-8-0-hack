764b5e05927a34afbaf0f0f4d56f976cd78feea2
Bongi Fleischer <bongifleischer@berkeley.edu>
2021-10-23 13:00:20 -0700

    added dependencies for uniswap liquidity pooling

--- packages/hardhat/contracts/LiquidityProvisionContract.sol
+++ packages/hardhat/contracts/LiquidityProvisionContract.sol
@@ -1,10 +1,22 @@
 pragma solidity >=0.8.0 <0.9.0;
+pragma abicoder v2;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-periphery/contracts//base/LiquidityManagement.sol";
 
-contract LiquidityProvisionContract is Ownable {
+contract LiquidityProvisionContract is
+    Ownable,
+    IERC721Receiver,
+    LiquidityManagement
+{
     //event SetPurpose(address sender, string purpose);
 
     string public purpose = "Automatic LP adjustement";
