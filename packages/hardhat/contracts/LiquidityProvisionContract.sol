8471e31ae89504f054c653a8512f492083ace43d
Bongi Fleischer <bongifleischer@berkeley.edu>
2021-10-23 18:22:09 -0700

    added fields to smart contract.

--- packages/hardhat/contracts/LiquidityProvisionContract.sol
+++ packages/hardhat/contracts/LiquidityProvisionContract.sol
@@ -7,15 +7,40 @@ import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZe
 contract LiquidityProvisionContract is Ownable {
     //event SetPurpose(address sender, string purpose);
 
-    string public purpose = "Automatic LP adjustement";
+    mapping(string => string) public strategies;
+
+    string public token1 = "ETH";
+    string public token2 = "DAI";
+    string public strategy = "fixed strategy";
+    string public purpose = "Auto LP Management";
 
     constructor() {
-        // what should we do on deploy?
+        strategies[
+            "fixed strategy"
+        ] = "Always provide liquidity in the price interval.";
+        strategies[
+            "uniform reset strategy"
+        ] = "Allocate liquidity uniformly on a range of bins centered on the current price. Reset when the price moves outside this range.";
+        strategies[
+            "proportional reset strategy"
+        ] = "Allocate liquidity proportionally to the probability of each bin in the that has 50% mass of the next-price distribution.";
     }
 
     function setPurpose(string memory newPurpose) public {
         purpose = newPurpose;
         console.log(msg.sender, "set purpose to", purpose);
+        //emit SetPurpose(msg.sender, purpose);
+    }
+
+    function setToken1(string memory newToken1) public {
+        token1 = newToken1;
+        console.log(msg.sender, "set token1 to", token1);
+        //emit SetPurpose(msg.sender, purpose);
+    }
+
+    function setToken2(string memory newToken2) public {
+        token2 = newToken2;
+        console.log(msg.sender, "set token1 to", token2);
         //emit SetPurpose(msg.sender, purpose);
     }
 }
